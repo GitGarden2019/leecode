a900c038587126603f60aace1e63f9d638f8b4e7
xuemin.zhu <zhuxueminvip@163.com>
2019-06-08 13:07:36 +0800

    代码提交

---
+++ src/com/leecode/Ls.java
@@ -0,0 +1,33 @@
+package com.leecode;
+
+public class Ls {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val=x;
+        }
+
+        /**
+         *
+         * @param val
+         */
+        public void insert(int val){
+            
+        }
+
+        /**
+         * 打印一下这个链表
+         */
+        public void print(){
+
+        }
+    }
+}
